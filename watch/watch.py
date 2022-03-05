46d5f51762863e07425bcab36e8793bfbf9b3ee8
ernzen1 <guilhermeernzen10@gmail.com>
2022-03-05 00:38:06 -0300

    eight

--- watch/watch.py
+++ watch/watch.py
@@ -1,11 +1,25 @@
+import time
+
 sec = 0
 minu = 0
 hour = 0
 
-while minu < 60:
-    print("minu =", minu)
-    minu += 1
-    sec = 0
-    while sec < 60:
-        print(sec)
+while True:
+    if hour < 24 and minu < 60 and sec < 60:
+        print(f"{hour}:{minu}:{sec}")
         sec += 1
+        time.sleep(1)
+        continue
+    elif hour < 24 and minu < 60:
+        sec = 0
+        minu += 1
+        continue
+    elif hour < 24:
+        minu = 0
+        hour += 1
+        continue
+    else:
+        hour = 0
+        minu = 0
+        sec = 0
+        break

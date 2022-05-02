ccf159069a81df83fd6d777255342c3b24fffe13
Ernzen1 <guilhermeernzen10@gmail.com>
2022-05-02 19:30:40 -0300

    Sixteen

---
+++ Curso/aula71/aula71.py
@@ -0,0 +1,22 @@
+# lists, tuples, str -> Sequences -> Iterável
+
+nome = "Guilherme Ernzen"
+iterador = iter(nome)  # Iteradores são consumíveis.
+
+print(next(iterador))
+print(next(iterador))
+print(next(iterador))
+print(next(iterador))
+print(next(iterador))
+print(next(iterador))
+print(next(iterador))
+#
+# for letra in nome:
+#     print(letra)
+#
+# print("#" * 80)
+#
+# for letra in nome:
+#     print(letra.lower())
+#
+
